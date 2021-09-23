b6b7978c492af64748e28bf8dea379c073cf6fbd
gunarp <gunarp@cs.washington.edu>
2021-09-23 11:21:22 -0700

    add constant text pulibhser

--- src/nautilus_text/scripts/pub_constant.py
+++ src/nautilus_text/scripts/pub_constant.py
@@ -2,11 +2,13 @@ import rospy
 from std_msgs.msg import String
 
 def main():
-  rospy.init_node('text_publisher', log_level=rospy.DEBUG)
+  rospy.init_node('text_publisher_constant', log_level=rospy.DEBUG)
   pub = rospy.Publisher('/nautilus/text', String, queue_size=10)
+  rate = rospy.Rate(60)
   while not rospy.is_shutdown():
-    msg = input("Enter message: ")
+    msg = str(rospy.get_time())
     pub.publish(msg)
+    rate.sleep()
 
 if __name__ == '__main__':
   main()
