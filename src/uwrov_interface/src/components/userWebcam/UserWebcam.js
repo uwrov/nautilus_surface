133bfce38c3a73bd4ed854d409ed879bd85087da
Kenneth Yang <kjy5@cs.washington.edu>
2021-11-07 14:45:48 -0800

    Added publisher code, added comments to UserWebcam.js, registered publisher in main_server

--- src/uwrov_interface/src/components/userWebcam/UserWebcam.js
+++ src/uwrov_interface/src/components/userWebcam/UserWebcam.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
+/**
+ * UserWebcam component. Access webcam, display it as a widget, and publish feed back to ROS
+ * @author: Kenneth Yang
+ * @version: 1.0.0
+ */
 export default class UserWebcam extends React.Component {
+    /**
+     * Create video src state and connect to SocketIO
+     * @constructor
+     */
     constructor(props) {
         super(props);
         this.socket = require('socket.io-client')('http://localhost:4040');
         this.state = {
-            videoSrc: null,
+            frame: null,
         }
     }
 
+    /**
+     * Get webcam stream and set it to video src. Set video to 480p.
+     */
     componentDidMount() {
         let videoConstraints = {
             video: {
@@ -20,6 +32,8 @@ export default class UserWebcam extends React.Component {
                 }
             }
         };
+
+        // Get video stream via getUserMedia
         navigator.mediaDevices.getUserMedia(videoConstraints).then(stream => {
             let video = document.querySelector('video');
             video.srcObject = stream;
@@ -28,6 +42,9 @@ export default class UserWebcam extends React.Component {
         });
     }
 
+    /**
+     * Display webcam as video element
+     */
     render() {
         return (
             <div className="user-webcam-video">
---
+++ src/uwrov_server/src/publishers/user_webcam_pub.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import rospy
+import threading
+from .publisher import ServerPub
+from sensor_msgs.msg import CompressedImage
+
+
+class UserWebcamPub(ServerPub):
+    """
+    Publisher for user webcam frames.
+    """
+
+    def __init__(self, topic):
+        super().__init__(topic, CompressedImage, queue_size=1)
+        self.msg = CompressedImage()
+        self.publisher_thread = None
+
+        self.launch_continuous_publisher()
+
+    def update_video_frame(self, state):
+        self.msg.header.stamp = rospy.Time.now()
+        self.msg.format = "jpeg"
+        self.msg.data = state.frame
+
+    def publish(self, **kwargs):
+        self.publisher.publish(self.msg)
+
+    def publish_continuously(self, state):
+        rate = rospy.Rate(30)  # 30Hz
+        while not rospy.is_shutdown():
+            self.publish()
+            rate.sleep()
+
+    def launch_continuous_publisher(self):
+        self.publisher_thread = threading.Thread(target=self.publish_continuously, args=(self,), daemon=True)
+        self.publisher_thread.start()
