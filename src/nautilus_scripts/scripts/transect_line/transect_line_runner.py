e8a10b477529576da3bfbe6a1dd6a451b28e717d
9KelvinN <9kelvin@gmail.com>
2022-01-17 18:27:59 -0800

    Added images and files for the transect line task

---
+++ src/nautilus_scripts/scripts/transect_line/transect_line_runner.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+ROS driver for the transect line task
+
+Authored by Kelvin Ng 
+some code from https://stackoverflow.com/questions/45322630/how-to-detect-lines-in-opencv
+"""
