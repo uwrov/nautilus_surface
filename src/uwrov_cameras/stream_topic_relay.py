2b4ebf083845b414582937e79c74af1cfefc6e2d
Angela Wei <awei9@uw.edu>
2022-05-23 20:02:18 -0700

    Updated stream topic relay for fisheye cameras

--- src/uwrov_cameras/stream_topic_relay.py
+++ src/uwrov_cameras/stream_topic_relay.py
@@ -4,10 +4,17 @@ import rospy
 import os
 from sensor_msgs.msg import CompressedImage
 import cv2
+import numpy as np
 
 def main():
-  ips_to_topics = {'192.168.0.99:8081' : '/nautilus/motion1',
-                   '192.168.0.99:8082' : '/nautilus/motion2'}
+  DIM=(640, 480)
+  K=np.array([[232.59834329560857, 0.0, 323.69056666007043], [0.0, 224.2212045670311, 249.45648108995732], [0.0, 0.0, 1.0]])
+  D=np.array([[-0.030436347449448193], [0.009118483708036189], [-0.03442166148483811], [0.014312258830753885]])
+
+  ips_to_topics = {'192.168.0.99:8081' : '/rov_camera/front',
+                   '192.168.0.99:8082' : '/rov_camera/down',
+                   '192.168.0.99:8083' : '/rov_camera/left',
+                   '192.168.0.99:8084' : '/rov_camera/right'}
 
   rospy.init_node('motion')
   pairs = {}
@@ -17,16 +24,26 @@ def main():
   for ip in ips_to_topics:
     stream = cv2.VideoCapture(ip)
     publisher = rospy.Publisher(ips_to_topics[ip], CompressedImage, queue_size=1)
-    pairs[stream] = publisher
+    if (ips_to_topics[ip] == '/rov_camera/front'):
+      pairs[stream] = (publisher, True)
+    else:
+      pairs[stream] = (publisher, False)
 
   msg = CompressedImage()
 
   while not rospy.is_shutdown():
     for stream in pairs:
       ret, frame = stream.read()
-      msg.data = frame
+      if pairs[stream][1]: 
+        h,w = frame.shape[:2]
+        map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, DIM, cv2.CV_16SC2)
+        undistorted_img = cv2.remap(frame, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+        msg.data = undistorted_img
+      else:
+        msg.data = frame
+
       if ret:
-        pairs[stream].publish(msg)
+        pairs[stream][0].publish(msg)
     rate.sleep()
 
 def shutdown_fn(pairs_ptr):
