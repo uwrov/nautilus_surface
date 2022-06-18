95168082277e8440c9d68188b9dc371609e63f7e
uwrov-github <uwrov@uw.edu>
2022-06-17 18:27:14 -0700

    changes

--- src/nautilus_scripts/scripts/go_forward/go_forward.py
+++ src/nautilus_scripts/scripts/go_forward/go_forward.py
@@ -7,16 +7,16 @@ robot = None
 
 def go_forward():
     for i in range(10):
-        robot.set_vel([0, -0.1, 0], [0, 0, 0])
+        robot.set_vel([0, -0.5, 0], [0, 0, 0])
         time.sleep(2)
         robot.set_vel([0, 0, 0], [0, 0, 0])
         time.sleep(1)
-        if i % 2 == 0:
-            robot.set_vel([0, 0, 0], [0, 0, -0.01])
-        else:
-            robot.set_vel([0, 0, 0], [0, 0, 0.01])
-        time.sleep(1)
-        robot.set_vel([0, 0, 0], [0, 0, 0])
+        #if i % 2 == 0:
+        #    robot.set_vel([0, 0, 0], [0, 0, -0.01])
+        #else:
+        #    robot.set_vel([0, 0, 0], [0, 0, 0.01])
+        #time.sleep(1)
+        #robot.set_vel([0, 0, 0], [0, 0, 0])
 
 
 def main():
--- src/uwrov_cameras/stream_topic_relay.py
+++ src/uwrov_cameras/stream_topic_relay.py
@@ -35,13 +35,14 @@ def main():
     for stream in pairs:
       try:
           ret, frame = stream.read()
+          print(type(frame))
           if pairs[stream][1]:
             h, w = frame.shape[:2]
             map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, DIM, cv2.CV_16SC2)
             undistorted_img = cv2.remap(frame, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
-            msg.data = undistorted_img
+            msg.data = undistorted_img.tolist()
           else:
-            msg.data = frame
+            msg.data = frame.tolist()
 
           if ret:
             pairs[stream][0].publish(msg)
